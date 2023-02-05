b2cc07d587504ab68a31b4f1909b9e5e05c015d1
crccode <54750603+crccode@users.noreply.github.com>
2023-02-04 22:15:02 -0400

    uSANDO libreria de Material IU
    https://mui.com/material-ui/getting-started/installation/
    
    Esta fallando el uso de la libreria

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import './Header.css';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+//import './Header.css';
+
+//Coloco menu bar, barra de herramienta, icono, Texto
+//Typography le decimos que use div y le pasamos css
 export default function Header(){
 	return (
 		<header>
-			<h1>
-				React Notes
-			</h1>
+			<AppBar position='static'>
+				<Toolbar>
+					<StickyNote2 sx={{mr: 2}} />
+					<Typography variant='h6' component="div" sx={{flexGrow: 1}}>
+						React Note
+					</Typography>
+				</Toolbar>
+			</AppBar>
 		</header>
 	);
 }
